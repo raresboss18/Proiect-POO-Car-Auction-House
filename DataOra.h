62ee1d7885381de9b581cfc98d69605a2d119e28
raresboss18 <raresionita@yahoo.ro>
2025-12-07 23:11:26 +0200

    DataOra.h

--- DataOra.h
+++ DataOra.h
@@ -21,12 +21,13 @@ private:
 public:
 
     explicit DataOra(int an = 2025, int luna = 1, int zi = 1, int ora = 12, int minut = 0, int secunda = 0);
-    static DataOra obtineOraCurenta();
+    DataOra obtineOraCurenta();
     friend std::ostream& operator<<(std::ostream& os, const DataOra& time);
     bool operator<(const DataOra& other) const;
     bool operator>(const DataOra& other) const;
     void adaugaMinute(int minuteAdaugate);
 };
+
 
 
 #endif //PROIECT_POO_DATAORA_H
